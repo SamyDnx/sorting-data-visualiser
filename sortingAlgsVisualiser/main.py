360ff7dbfc622aeac988da47ee590e988e9ec86e
SamyDnx <doneux.samy.uni@gmail.com>
2024-10-06 14:52:55 +0200

    Cleaned up game loop

--- sortingAlgsVisualiser/main.py
+++ sortingAlgsVisualiser/main.py
@@ -24,21 +24,25 @@ def generate_random_array(length=ARRAY_LENGTH):
         array[i] += random.randint(1, 100)
     return array
 
+
 def generate_sorted_array():
     array = generate_random_array()
     array.sort()
     return array
 
+
 def generate_reversed_array():
     array = generate_sorted_array()
     return array[::-1]
 
+
 def draw_data_set(array, color=WHITE):
     length = len(array)
     for i in range(length):
-        data_rect = pygame.Rect((CELL_SIZE*(i+1), 0, CELL_SIZE, CELL_SIZE*array[i]))
+        data_rect = pygame.Rect((CELL_SIZE * (i + 1), 0, CELL_SIZE, CELL_SIZE * array[i]))
         pygame.draw.rect(window, color, data_rect)
         pygame.display.flip()
+
 
 def draw_bubble_sort(array):
     n = len(array)
@@ -50,19 +54,19 @@ def draw_bubble_sort(array):
                     pygame.quit()
                     exit()
 
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
 
-                rect1 = pygame.Rect(CELL_SIZE*(j+1), 0, CELL_SIZE, HEIGHT)
-                rect2 = pygame.Rect(CELL_SIZE*(j+2), 0, CELL_SIZE, HEIGHT)
+                rect1 = pygame.Rect(CELL_SIZE * (j + 1), 0, CELL_SIZE, HEIGHT)
+                rect2 = pygame.Rect(CELL_SIZE * (j + 2), 0, CELL_SIZE, HEIGHT)
 
                 window.fill(BLACK, rect1)
                 window.fill(BLACK, rect2)
 
                 pygame.display.flip()
 
-                n_rect1 = pygame.Rect(CELL_SIZE*(j+1), 0, CELL_SIZE, (CELL_SIZE*array[j]))
-                n_rect2 = pygame.Rect(CELL_SIZE*(j+2), 0, CELL_SIZE, (CELL_SIZE*array[j+1]))
+                n_rect1 = pygame.Rect(CELL_SIZE * (j + 1), 0, CELL_SIZE, (CELL_SIZE * array[j]))
+                n_rect2 = pygame.Rect(CELL_SIZE * (j + 2), 0, CELL_SIZE, (CELL_SIZE * array[j + 1]))
 
                 pygame.draw.rect(window, WHITE, n_rect1)
                 pygame.draw.rect(window, WHITE, n_rect2)
@@ -71,34 +75,35 @@ def draw_bubble_sort(array):
 
                 pygame.time.delay(1)
 
-        window.fill(GREEN, pygame.Rect(CELL_SIZE*(n-i), 0, CELL_SIZE, (CELL_SIZE*array[n-1-i])))
+        window.fill(GREEN, pygame.Rect(CELL_SIZE * (n - i), 0, CELL_SIZE, (CELL_SIZE * array[n - 1 - i])))
         pygame.display.flip()
         pygame.time.delay(1)
+
 
 def draw_insertion_sort(array):
     n = ARRAY_LENGTH
     i = 0
     while i < n:
         j = i
-        while j > 0 and array[j-1] > array[j]:
+        while j > 0 and array[j - 1] > array[j]:
             # 100% useless, it's just to keep the os responsive
             for ev in pygame.event.get():
                 if ev.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-            
-            array[j-1], array[j] = array[j], array[j-1]
 
-            rect1 = pygame.Rect(CELL_SIZE*(j), 0, CELL_SIZE, HEIGHT)
-            rect2 = pygame.Rect(CELL_SIZE*(j+1), 0, CELL_SIZE, HEIGHT)
+            array[j - 1], array[j] = array[j], array[j - 1]
+
+            rect1 = pygame.Rect(CELL_SIZE * j, 0, CELL_SIZE, HEIGHT)
+            rect2 = pygame.Rect(CELL_SIZE * (j + 1), 0, CELL_SIZE, HEIGHT)
 
             window.fill(BLACK, rect1)
             window.fill(BLACK, rect2)
 
             pygame.display.flip()
 
-            n_rect1 = pygame.Rect(CELL_SIZE*(j), 0, CELL_SIZE, (CELL_SIZE*array[j-1]))
-            n_rect2 = pygame.Rect(CELL_SIZE*(j+1), 0, CELL_SIZE, (CELL_SIZE*array[j]))
+            n_rect1 = pygame.Rect(CELL_SIZE * j, 0, CELL_SIZE, (CELL_SIZE * array[j - 1]))
+            n_rect2 = pygame.Rect(CELL_SIZE * (j + 1), 0, CELL_SIZE, (CELL_SIZE * array[j]))
 
             pygame.draw.rect(window, WHITE, n_rect1)
             pygame.draw.rect(window, WHITE, n_rect2)
@@ -110,14 +115,17 @@ def draw_insertion_sort(array):
             j -= 1
 
         i += 1
-    
+
     draw_data_set(array, GREEN)
+
 
 def draw_merge(array, l, m, r):
     ...
 
+
 def draw_merge_sort(array, l, r):
     ...
+
 
 def ending_animation(array, color):
     n = len(array)
@@ -129,7 +137,7 @@ def ending_animation(array, color):
                 pygame.quit()
                 exit()
 
-        rect = pygame.Rect((CELL_SIZE*(i+1), 0, CELL_SIZE, CELL_SIZE*array[i]))
+        rect = pygame.Rect((CELL_SIZE * (i + 1), 0, CELL_SIZE, CELL_SIZE * array[i]))
         pygame.draw.rect(window, color, rect)
         pygame.display.flip()
         pygame.time.delay(10)
@@ -142,14 +150,17 @@ def show_sorting_data(array, sort="b"):
             draw_bubble_sort(array)
         case "i":
             draw_insertion_sort(array)
-        case "m":
-            draw_merge_sort(array)
+        #case "m":
+            #draw_merge_sort(array)
 
 
 run = True
-first_itr = False
 sort_type = "b"
+arr = generate_random_array(ARRAY_LENGTH)
+draw_data_set(arr)
+
 while run:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -160,13 +171,13 @@ while run:
                 draw_data_set(arr)
                 show_sorting_data(arr, sort_type)
                 ending_animation(arr, BLUE)
-                
+
             elif event.key == pygame.K_r:
                 window.fill(BLACK)
                 pygame.display.flip()
                 arr = generate_random_array(ARRAY_LENGTH)
                 draw_data_set(arr)
-            
+
             elif event.key == pygame.K_RETURN:
                 window.fill(BLACK)
                 pygame.display.flip()
@@ -178,16 +189,10 @@ while run:
                 pygame.display.flip()
                 arr = generate_reversed_array()
                 draw_data_set(arr)
-            
+
             elif event.key == pygame.K_b:
                 sort_type = "b"
             elif event.key == pygame.K_i:
                 sort_type = "i"
             elif event.key == pygame.K_m:
                 sort_type = "m"
-                
-
-    if not first_itr:
-        first_itr = True
-        arr = generate_random_array(ARRAY_LENGTH)
-        draw_data_set(arr)
